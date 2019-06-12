eae04e9901cbfaaab6826f8247fe9c6d9ec6b72b
Tommy Pyatt <tommy@space48.com>
2019-06-12 13:38:13 +0100

    Tidy up styling etc

--- src/components/Holiday.js
+++ src/components/Holiday.js
@@ -12,21 +12,26 @@ class Holiday extends React.Component {
     render () {
         return (
             <li className='result-item'>
-                <div className='image'>
-                    <label htmlFor={'view-more-' + this.props.result.id}>View more</label>
-                </div>
-                <div className='info'>
-                    <h2>{this.props.result.name}</h2>
-                    <h3>Costa Adeje, Tenerife</h3>
-                    <div className='rating-outer'>
-                        <div className='rating-inner' style={{width: (this.props.result.rating * 20) + '%'}}></div>
+                <div className='item-outer'>
+                    <div className='image'>
+                        <img src={`images/hotel-images/${this.props.result.image}`} />
                     </div>
-                    <p className='humans'>2 Adults, 2 Children</p>
-                    <a>
-                        <span>Book now</span>
-                        <span className='price'>{this.priceFormat(this.props.result.price)}</span>
-                    </a>
+                    <div className='info'>
+                        <h2>{this.props.result.name}</h2>
+                        <h3>Costa Adeje, Tenerife</h3>
+                        <div className='rating-outer'>
+                            <div className='rating-inner' style={{width: (this.props.result.rating * 20) + '%'}}></div>
+                        </div>
+                        <p className='humans'>2 Adults, 2 Children</p>
+                        <a className='book-button'>
+                            <span>Book now</span>
+                            <span className='price'>{this.priceFormat(this.props.result.price)}</span>
+                        </a>
+                    </div>
+                </div>
+                <div className='item-viewmore'>
                     <input className="view-checkbox" type="checkbox" id={'view-more-' + this.props.result.id} />
+                    <label htmlFor={'view-more-' + this.props.result.id}>View more</label>
                     <p className="description">Lorem ipsum</p>
                 </div>
             </li>
--- src/components/Sorter.js
+++ src/components/Sorter.js
@@ -13,8 +13,8 @@ class Sorter extends React.Component {
         return (
             <div className='sorter'>
                 <ul onClick={this.clearExpanded}>
+                    <li className={this.props.sortedBy === 'name' ? 'selected' : ''} onClick={() => this.props.sortBy('name')}>Sort alphabetically</li>
                     <li className={this.props.sortedBy === 'price' ? 'selected' : ''} onClick={() => this.props.sortBy('price')}>Sort by price</li>
-                    <li className={this.props.sortedBy === 'name' ? 'selected' : ''} onClick={() => this.props.sortBy('name')}>Sort by name</li>
                     <li className={this.props.sortedBy === 'rating' ? 'selected' : ''} onClick={() => this.props.sortBy('rating')}>Sort by star rating</li>
                 </ul>
             </div>
